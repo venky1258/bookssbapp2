6806305eacc8b0d2e3c7a0fb70f934fd0912148a
venky1258 <140711690+venky1258@users.noreply.github.com>
2024-04-17 11:49:31 +0530

    code update1

---
+++ BooksRepository.java
@@ -0,0 +1,12 @@
+package com.mypack.bookssb1;
+
+import org.springframework.data.repository.CrudRepository;  
+  
+ 
+public interface BooksRepository extends CrudRepository<Books, Integer>  
+{  
+	
+	
+	
+}  
+
